c40aec7d3afc9271a20288862d224609a7d57ce7
Alexander Moran <j-moran@optusnet.com.au>
2018-11-19 00:10:18 +1000

    Tidy up and Console fixes
    import sanitation

--- NameSorterConsole/Program.cs
+++ NameSorterConsole/Program.cs
@@ -1,29 +1,44 @@
 using NameSorter;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NameSorterConsole
 {
+    /// <summary>
+    /// Program to Sort a list of names, output to screen and print to a file
+    /// </summary>
     class Program
     {
-        private static readonly string openPath = "unsorted-names-list.txt";
         private static readonly string savePath = "sorted-names-list.txt";
 
         static void Main(string[] args)
         {
-            string[] input = System.IO.File.ReadAllLines(openPath);
+            //Input
+            if (args.Length == 0 || !System.IO.File.Exists(args[0]))
+            {
+                Console.WriteLine("Invaild File Provided, Please Try Again");
+                return;
+            }
+            string[] input = System.IO.File.ReadAllLines(args[0]);
 
+            //Sorting
             List<Name> names = new List<Name>();
             foreach (string fullName in input)
             {
-                names.Add(new Name(fullName));
+                try
+                {
+                    names.Add(new Name(fullName));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error Reading Name: " + fullName);
+                }
             }
             names.Sort();
 
+            //OutPut
             string sortedNames = string.Join<Name>(Environment.NewLine, names.ToArray());
+            Console.WriteLine();
             Console.Write(sortedNames);
             System.IO.File.WriteAllText(savePath, sortedNames);
 
